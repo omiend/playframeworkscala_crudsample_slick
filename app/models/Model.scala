9ddbde489f86fa5f1ff4ff5d48757aca944eaca8
omiend <omiend@gmail.com>
2014-11-03 23:34:44 +0900

    Slick Sample仕掛かり中

--- app/models/Model.scala
+++ app/models/Model.scala
@@ -2,19 +2,10 @@ package models
 
 import java.sql.Timestamp
 
-import play.api.db.DB
-import play.api.Play.current
-
 import scala.slick.driver.MySQLDriver.simple._
-import scala.slick.jdbc.meta.{ MTable, createModel }
-import scala.slick.driver.JdbcDriver
 
-
+/** Parent */
 case class Parent(id: Long, name: String, createDate: Timestamp, updateDate: Timestamp)
-
-/**
- * ③ テーブルスキーマの定義
- */
 class ParentTable(tag: Tag) extends Table[Parent](tag, "parent") {
   def id         = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def name       = column[String]("name", O.NotNull)
@@ -22,18 +13,31 @@ class ParentTable(tag: Tag) extends Table[Parent](tag, "parent") {
   def updateDate = column[Timestamp]("update_date")
   def * = (id, name, createDate, updateDate) <> ((Parent.apply _).tupled, Parent.unapply)
 }
-
 object Parent {
-
 	// DDL  
   lazy val query = TableQuery[ParentTable]
+	// 全件取得
+  def findAll()(implicit s: Session): List[Parent] = query.list
+	// 全件数取得
+  def count()(implicit s: Session): Int = query.list.size
+}
 
+/** Child */
+case class Child(id: Long, name: String, parentId: Long, createDate: Timestamp, updateDate: Timestamp)
+class ChildTable(tag: Tag) extends Table[Child](tag, "child") {
+  def id         = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def name       = column[String]("name", O.NotNull)
+  def parentId   = column[Long]("parent_id")
+  def createDate = column[Timestamp]("create_date")
+  def updateDate = column[Timestamp]("update_date")
+  def * = (id, name, parentId, createDate, updateDate) <> ((Child.apply _).tupled, Child.unapply)
+  def parent = foreignKey("parent_id", parentId, Parent.query)(_.id)
+}
+object Child {
+	// DDL  
+  lazy val query = TableQuery[ChildTable]
 	// 全件取得
-  def findAll()(implicit s: Session): List[Parent] = {
-    query.list
-  }
-	// 全件取得
-  def count()(implicit s: Session): Int = {
-    query.list.size
-  }
+  def findAll()(implicit s: Session): List[Child] = query.list
+	// 全件数取得
+  def count()(implicit s: Session): Int = query.list.size
 }
