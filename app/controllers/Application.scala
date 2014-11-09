2d9d87141294fa1aafd74b67061540bf9cbba4b0
omiend <omiend@gmail.com>
2014-11-09 22:43:07 +0900

    JodaTime

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,20 +1,79 @@
 package controllers
 
-import java.sql.Timestamp
+// import java.sql.Timestamp
+// val ts: Timestamp = new Timestamp(System.currentTimeMillis());
 
 import play.api._
 import play.api.mvc._
-
-import play.api.db.slick._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formats._
 
 import models._
 
+import play.api.db.slick._
+
 object Application extends Controller {
 
-  def index(pageNum: Int) = DBAction { implicit request =>
-  	val pager: Pager[Parent] = Pager[Parent]("トップ", pageNum, Parent.count, Parent.findAll())
-  	val ts: Timestamp = new Timestamp(System.currentTimeMillis());
-	// Parent.insert(new Parent(0, "TEST", ts, ts))
-    Ok(views.html.index(pager))
+  def parent(pageNum: Int) = DBAction { implicit request =>
+  	val pager: Pager[Parent] = Pager[Parent]("トップ", pageNum, Parent.count, Seq.empty)
+  	pager.dataList = Parent.findOffset((pager.pageNum * pager.maxListCount - pager.maxListCount), pager.maxListCount)
+    Ok(views.html.parent(pager))
+  }
+  def child(pageNum: Int) = DBAction { implicit request =>
+  	val pager: Pager[(Child, Parent)] = Pager[(Child, Parent)]("トップ", pageNum, Child.count, Seq.empty)
+  	pager.dataList = Child.findByIdWithParent((pager.pageNum * pager.maxListCount - pager.maxListCount), pager.maxListCount)
+    Ok(views.html.child(pager))
+  }
+
+  /** Parent form */
+  val parentForm = Form(
+    mapping(
+      "id" -> ignored(None: Option[Long]),
+      "name" -> nonEmptyText,
+      "createDate" -> jodaDate,
+      "updateDate" -> jodaDate
+    )(Parent.apply)(Parent.unapply)
+  )
+
+  def createParent(pageNum: Int) = DBAction { implicit request =>
+    val pager: Pager[Parent] = Pager[Parent]("トップ", pageNum, Parent.count, Seq.empty)
+    Ok(views.html.createParent(pager, parentForm))
+  }
+
+  // def insertParent(pageNum: Int) {
+  //   val parent = parentForm.bindFromRequest.get
+
+  //       parentForm.bindFromRequest.fold(
+  //         formWithErrors => {
+  //           BadRequest(html.parent(pageNum, formWithErrors))
+  //         },
+  //         festivalAndStage => {
+  //         }
+
+
+
+
+  //   Parent.insert(parent)
+  //   Redirect(routes.Application.parent(pageNum))
+  // }
+
+  /** Child form */
+  val childForm = Form(
+    mapping(
+      "id" -> ignored(None: Option[Long]),
+      "name" -> nonEmptyText,
+      "parentId" -> ignored(None: Option[Long]),
+      "createDate" -> jodaDate,
+      "updateDate" -> jodaDate
+    )(Child.apply)(Child.unapply)
+  )
+
+  def pageNation(pageNum: Int, page: String) = Action { implicit request =>
+    page match {
+      case "parent" => Redirect(routes.Application.parent(pageNum))
+      case "child"  => Redirect(routes.Application.child(pageNum))
+      case _        => Redirect(routes.Application.parent(pageNum))
+    }
   }
 }
--- app/models/Model.scala
+++ app/models/Model.scala
@@ -1,43 +1,64 @@
 package models
 
-import java.sql.Timestamp
-
 import scala.slick.driver.MySQLDriver.simple._
 
+import org.joda.time.DateTime
+import java.sql.Timestamp
+// import JodaToSqlMapper._
+
 /** Parent */
-case class Parent(id: Long, name: String, createDate: Timestamp, updateDate: Timestamp)
-class ParentTable(tag: Tag) extends Table[Parent](tag, "parent") {
-  def id         = column[Long]("id", O.PrimaryKey, O.AutoInc)
-  def name       = column[String]("name", O.NotNull)
-  def createDate = column[Timestamp]("create_date")
-  def updateDate = column[Timestamp]("update_date")
-  def * = (id, name, createDate, updateDate) <> ((Parent.apply _).tupled, Parent.unapply)
-}
+case class Parent(id: Option[Long] = None, name: String, createDate: org.joda.time.DateTime, updateDate: org.joda.time.DateTime)
 object Parent {
+	class ParentTable(tag: Tag) extends Table[Parent](tag, "parent") {
+	  def id         = column[Long]("id", O.PrimaryKey, O.AutoInc)
+	  def name       = column[String]("name", O.NotNull)
+	  def createDate = column[DateTime]("create_date")
+	  def updateDate = column[DateTime]("update_date")
+	  def * = (id, name, createDate, updateDate) <> ((Parent.apply _).tupled, Parent.unapply)
+	}
 	// DDL  
-  lazy val query = TableQuery[ParentTable]
+  lazy val parentQuery = TableQuery[ParentTable]
 	// 全件取得
-  def findAll()(implicit s: Session): List[Parent] = query.list
+  def findAll()(implicit s: Session): List[Parent] = parentQuery.list
 	// 全件数取得
-  def count()(implicit s: Session): Int = query.list.size
+  def count()(implicit s: Session): Int = parentQuery.list.size
+  // Offset取得
+  def findOffset(offset: Int, limit: Int)(implicit s: Session): List[Parent] = parentQuery.drop(offset).take(limit).list
+  def findOffsetAndChildList(offset: Int, limit: Int)(implicit s: Session): List[Parent] = parentQuery.drop(offset).take(limit).list
 }
 
 /** Child */
-case class Child(id: Long, name: String, parentId: Long, createDate: Timestamp, updateDate: Timestamp)
-class ChildTable(tag: Tag) extends Table[Child](tag, "child") {
-  def id         = column[Long]("id", O.PrimaryKey, O.AutoInc)
-  def name       = column[String]("name", O.NotNull)
-  def parentId   = column[Long]("parent_id")
-  def createDate = column[Timestamp]("create_date")
-  def updateDate = column[Timestamp]("update_date")
-  def * = (id, name, parentId, createDate, updateDate) <> ((Child.apply _).tupled, Child.unapply)
-  def parent = foreignKey("parent_id", parentId, Parent.query)(_.id)
-}
+case class Child(id: Option[Long] = None, name: String, parentId: Option[Long] = None, createDate: org.joda.time.DateTime, updateDate: org.joda.time.DateTime)
 object Child {
+	class ChildTable(tag: Tag) extends Table[Child](tag, "child") {
+	  def id         = column[Long]("id", O.PrimaryKey, O.AutoInc)
+	  def name       = column[String]("name", O.NotNull)
+	  def parentId   = column[Long]("parent_id")
+	  def createDate = column[DateTime]("create_date")
+	  def updateDate = column[DateTime]("update_date")
+	  def * = (id, name, parentId, createDate, updateDate) <> ((Child.apply _).tupled, Child.unapply)
+	  def parent = foreignKey("parent_id", parentId, Parent.parentQuery)(_.id)
+	}
 	// DDL  
-  lazy val query = TableQuery[ChildTable]
+  lazy val childQuery = TableQuery[ChildTable]
+  lazy val parentQuery = TableQuery[Parent.ParentTable]
 	// 全件取得
-  def findAll()(implicit s: Session): List[Child] = query.list
+  def findAll()(implicit s: Session): List[Child] = childQuery.list
 	// 全件数取得
-  def count()(implicit s: Session): Int = query.list.size
+  def count()(implicit s: Session): Int = childQuery.list.size
+  // 親Idを指定して取得
+  def findByParentId(parentId: Long)(implicit s: Session): List[Child] = childQuery.filter(_.parentId === parentId).list
+  // 親レコードを結合して取得
+  def findByIdWithParent(offset: Int, limit: Int)(implicit s: Session): List[(Child, Parent)] = {
+  	(for {
+		  c <- childQuery
+		  p <- parentQuery if c.parentId === p.id
+		} yield (c, p)).drop(offset).take(limit).list
+  }
+}
+
+object JodaToSqlMapper {
+  implicit val dateTimeToDate = MappedColumnType.base[DateTime, Timestamp](
+    dateTime => new Timestamp(dateTime.getMillis),
+    timestamp => new DateTime(timestamp))
 }
